1a4c4bc302758180faa98550d01b1f8ea34c47c4
Luis Palacio <siulacio@hotmail.com>
2023-11-26 20:45:14 -0500

    Procesar formulario de contacto enviando correo con proveedor mailtrap

--- 01.contacto/procesar.php
+++ 01.contacto/procesar.php
@@ -1,5 +1,9 @@
 <?php
 
+require 'vendor/autoload.php';
+require __DIR__ . '/EmailService.php';
+require __DIR__ . '/MailtrapProvider.php';
+
 if (empty($_POST['nombre']) || empty($_POST['email']) || empty($_POST['mensaje'])) {
     header('Location: formulario.php?error=1');
     exit;
@@ -9,4 +13,19 @@ if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
     header('Location: formulario.php?error=2');
     exit;
 }
+
+$emailService = new EmailService(new MailtrapProvider);
+
+$sentEmail = $emailService->sendEmail(
+    to: $_POST['email'],
+    subject: 'Formulario de contacto',
+    body: $_POST['mensaje'],
+);
+
+if ($sentEmail) {
+    header('Location: formulario.php?success=1');
+    exit;
+}
+
+header('Location: formulario.php?error=3');
 
